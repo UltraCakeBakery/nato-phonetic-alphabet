095b938600d07857c5b3fc79f91c1799178d2f86
Jack van der Bilt <jack@managing.software>
2021-11-29 22:21:36 +0100

    feat!: include `es5` build and minify code

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,16 @@
 import { Options } from 'tsup'
 
 const config: Options = {
-    splitting: false,
-    format: ['esm', 'cjs'],
-    entryPoints: ['src/index.ts'],
-    target: 'es2015',
-    clean: true,
-    dts: true
+	splitting: false,
+	format: [ 'esm', 'cjs' ],
+	entryPoints: [ 'src/index.ts' ],
+	target: 'es5',
+	clean: true,
+	dts: true,
+	minify: true,
+	env: {
+		NODE_ENV: 'production'
+	}
 }
 
 export default config
